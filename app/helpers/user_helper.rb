4f8bb2773174c96a44f788e50240735bc743b41c
davidkelley <david.james.kelley@gmail.com>
2015-08-16 17:45:51 +0100

    added integration tests

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,8 @@ module UserHelper
   def header_partial
     @user ? 'authenticated' : 'anonymous'
   end
+
+  def logged_in?
+    @user != nil
+  end
 end
--- spec/integration/root_spec.rb
+++ spec/integration/root_spec.rb
@@ -1,27 +1,59 @@
 require 'rails_helper'
 
-describe 'home page' do
+describe 'root page' do
+  let(:time) { Time.now.utc }
 
-  before { visit '/' }
+  let(:yesterday) { Time.now.utc - 60*60*24 }
+
+  before { visit root_path }
 
   subject { page }
 
-  it { should has_css?('navbar navbar-default') }
+  it { should have_title 'Recent News' }
 
-  it { should has_css?('jumbotron') }
+  it { should have_title time.strftime("#{time.day.ordinalize} %B %Y") }
 
-  it { should has_css?('nav nav-pills') }
+  it { should have_content time.strftime("#{time.day.ordinalize} %B %Y") }
 
-  it { should has_css?('breaker') }
+  it { should have_content yesterday.strftime("#{yesterday.day.ordinalize} %B %Y") }
 
-  # it { should has_css?('h4', :text => 'Share').to be_truthy }
+  specify { expect(all('article').size).to be > 0 }
 
-  describe 'the header bar' do
+  describe 'when Yesterday is clicked' do
+    let(:previous_day) { yesterday - 60*60*24 }
 
-    specify { expect(subject.has_css?('h4', :text => 'Share')).to be_truthy }
+    before { first(:link, yesterday.strftime("#{yesterday.day.ordinalize} %B %Y")).click }
 
+    it { should have_title yesterday.strftime("#{yesterday.day.ordinalize} %B %Y") }
+
+    it { should have_content yesterday.strftime("#{yesterday.day.ordinalize} %B %Y") }
+
+    it { should have_content time.strftime("#{time.day.ordinalize} %B %Y") }
+
+    it { should have_content previous_day.strftime("#{previous_day.day.ordinalize} %B %Y") }
   end
 
+  describe 'when Join for Free is clicked' do
+    before { first(:link, 'Join for Free').click }
 
+    it { should have_title 'Register' }
+  end
+
+  describe 'when Login is clicked' do
+    before { first(:link, 'Login').click }
+
+    it { should have_title 'Login' }
+  end
+
+  describe 'when Popular News is clicked' do
+    before { first(:link, 'Top News').click }
+
+    it { should have_title 'Popular News' }
+  end
+
+  describe 'when Recent News is clicked' do
+    before { first(:link, 'Recent News').click }
+
+    it { should have_title 'Recent News' }
+  end
 end
-
